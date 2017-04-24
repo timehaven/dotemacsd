77de9ed2a580b7ef0b0610936c02796df05dbf6f
Ryan Woodard <ryan@timehaven.org>
2017-04-24 13:44:15 -0700

    Add some keys and colors

--- custom-settings.el
+++ custom-settings.el
@@ -3,7 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (magit auto-compile use-package))))
+ '(package-selected-packages
+   (quote
+    (color-theme-solarized color-theme magit auto-compile use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
