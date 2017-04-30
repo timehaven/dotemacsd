e835672daee20185a02cd567079767c11399334d
Ryan Woodard <rwoodard@appnexus.com>
2017-04-30 00:49:28 -0700

    Remove lots of elpa things that should not be saved

--- elpa/ob-ipython-20160918.1001/ob-ipython-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; ob-ipython-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("ob-ipython-pkg.el" "ob-ipython.el") (22789
-;;;;;;  36346 0 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; ob-ipython-autoloads.el ends here
