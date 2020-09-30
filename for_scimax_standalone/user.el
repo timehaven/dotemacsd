2d257d2276efd3052b6fd95e4599aaf4a0356c66
Ryan Woodard <ryan.woodard@wearethorn.org>
2020-09-29 18:40:51 -0700

    Make tweaks for Thorn box

--- for_scimax_standalone/user.el
+++ for_scimax_standalone/user.el
@@ -3,5 +3,10 @@
 ;; (org-babel-load-file (concat user-emacs-directory "emacs.org"))
 ;; (org-babel-load-file (expand-file-name "emacs.org" user-emacs-directory))
 
-(org-babel-load-file
- (expand-file-name "ryan_after_scimax.org" user-emacs-directory))
+;; (org-babel-load-file
+;;  (expand-file-name "ryan_after_scimax.org" user-emacs-directory))
+
+(setq my/startupstuff "~/github/timehaven/dotemacsd/for_scimax_standalone/ryan_after_scimax.org")
+(message (concat "org-babel-loading file " my/startupstuff))
+(org-babel-load-file my/startupstuff)
+(message "Completed babeling.")
